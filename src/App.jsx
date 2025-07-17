780e738a92e7a0b14b6a7ff8359f0b2cbdd28f12
Jorge-coder02 <jorgitenis@hotmail.es>
2025-07-17 14:18:41 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Navbar from "./components/header/Navbar";
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-100 text-gray-900">
+    <div className="min-h-[calc(100dvh-64px)] bg-gray-100 text-gray-900">
       <Navbar />
       <AppRoutes />
     </div>
