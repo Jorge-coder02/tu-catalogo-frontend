a8838a9fa728b021420fe12e76078a65fec16a35
Jorge-coder02 <jorgitenis@hotmail.es>
2025-07-23 12:46:37 +0200

    pendiente de actualizar estados 2

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -30,6 +30,9 @@ const authSlice = createSlice({
       state.user = null;
       state.token = null;
       state.error = null;
+
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
     },
   },
 });
