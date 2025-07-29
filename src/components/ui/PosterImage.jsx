34d9977c8b667ce93a5e19d4f43b8c65fdcac2e9
Jorge-coder02 <jorgitenis@hotmail.es>
2025-07-29 10:30:55 +0200

    fix: localstorage para el theme persistente

--- src/components/ui/PosterImage.jsx
+++ src/components/ui/PosterImage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 // PosterImage.jsx
 export default function PosterImage({ src, alt, onClick, className = "" }) {
-  const fallback = "public/img/imgnotfound.jpg";
+  const fallback = "/img/imgnotfound.jpg";
   const [imgSrc, setImgSrc] = useState(src !== "N/A" ? src : fallback);
 
   return (
@@ -11,7 +11,7 @@ export default function PosterImage({ src, alt, onClick, className = "" }) {
       src={imgSrc}
       alt={alt}
       onError={() => setImgSrc(fallback)}
-      className={`object-cover rounded w-full h-full ${className}`}
+      className={`object-cover rounded w-full h-full ${className} dark:hover:opacity-60 hover:opacity-80`}
     />
   );
 }
